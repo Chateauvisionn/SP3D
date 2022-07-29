448d7918d5e04d26d8c645ed56a200e80ae2ac76
Chateauvisionn <chateauvision@outlook.fr>
2022-07-29 19:10:45 +0200

    working audio

--- sp3d/audio.py
+++ sp3d/audio.py
@@ -1,14 +1,20 @@
 from sp3d import *
-from panda3d.core import *
 
 class Audio():
-    def __init__(self, audio_file):
+    def __init__(self, audio_file, volume=1.0):
         self.audio_file = audio_file
+        self.audio_node = loader.loadSfx(self.audio_file)
+        self._volume = volume
+        self.audio_node.setVolume(self._volume)
 
     def play(self, loop=False):
-        self.audio_node = AudioNode(self.audio_file)
-        self.audio_node.loop(loop)
-        self.audio_node.play()
+        self.audio_node.setLoop(loop)
+        if self.audio_node.status == self.audio_node.READY:
+            self.audio_node.play()
     
     def stop(self):
-        self.audio_node.stop()
+        if self.audio_node.status() == self.audio_node.PLAYING:
+            self.audio_node.stop()
+
+    def get_audio_length(self):
+        return self.audio_node.length()
